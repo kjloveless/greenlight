d5b3ae802b8d254e45fd89b595644889dbe7ac15
Kyle Loveless <kjloveless321@gmail.com>
2025-03-07 15:28:32 -0600

    return json for errors, pretty print, etc.

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,19 +5,19 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-  // Create a map which holds the information that we want to send in the
-  // response.
-  data := map[string]string{
+  // Declare an envelope map containing the data for the response. Notice that
+  // the way we've constructed this means the environment and version data will
+  // now be nested under a system_info key in the JSON response.
+  env := envelope{
     "status":       "available",
-    "environment":  app.config.env,
-    "version":      version,
+    "system_info": map[string]string{
+      "environment":  app.config.env,
+      "version":      version,
+    },
   }
   
-  err := app.writeJSON(w, http.StatusOK, data, nil)
+  err := app.writeJSON(w, http.StatusOK, env, nil)
   if err != nil {
-    app.logger.Error(err.Error())
-    http.Error(w, 
-      "the server encountered a problem and could not process your request",
-      http.StatusInternalServerError)
+    app.serverErrorResponse(w, r, err)
   }
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,6 +9,9 @@ import (
   "github.com/julienschmidt/httprouter"
 )
 
+// Define an envelope type.
+type envelope map[string]any
+
 // Retrieve the "id" URL parameter from the current request context, then
 // convert it to an integer and return it. If the operation isn't successful,
 // return 0 and an error.
@@ -30,10 +33,14 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 func (app *application) writeJSON(
   w http.ResponseWriter, 
   status int, 
-  data any,
-  headers http.Header) error {
+  data envelope,
+  headers http.Header,
+) error {
   // Encode the data to JSON, returning the error if there was one.
-  js, err := json.Marshal(data)
+  // Use the json.MarshalIndent() function so that whitespace is added to the
+  // encoded JSON. Here we use no line prefix ("") and tab indents ("\t") for
+  // each element.
+  js, err := json.MarshalIndent(data, "", "    ")
   if err != nil {
     return err
   }
