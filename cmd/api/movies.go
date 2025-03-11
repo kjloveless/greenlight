d58e552236d53b31a160a4ac8a394c63f93ffa55
Kyle Loveless <kjloveless321@gmail.com>
2025-03-11 11:53:28 -0500

    add validation to filters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -268,11 +268,16 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
   // provided by the client (which will imply a ascending sort on movie ID).
   // Read the sort query string value into the embedded struct.
   input.Filters.Sort = app.readString(qs, "sort", "id")
+  // Add the supported sort values for this endpoint to the sort safelist.
+  input.Filters.SortSafeList = []string{
+    "id", "title", "year", "runtime",
+    "-id", "-title", "-year", "-runtime",
+  }
 
   // Check the Validator instance for any errors and use the
   // failedValidationResponse() helper to send the client a response if
   // necessary.
-  if !v.Valid() {
+  if data.ValidateFilters(v, input.Filters); !v.Valid() {
     app.failedValidationResponse(w, r, v.Errors)
     return
   }
--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,25 @@
 package data
 
+import (
+  "github.com/kjloveless/greenlight/internal/validator"
+)
+
+// Add a SortSafeList field to hold the supported sort values.
 type Filters struct {
-  Page      int
-  PageSize  int
-  Sort      string
+  Page          int
+  PageSize      int
+  Sort          string
+  SortSafeList  []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+  // Check that the page and page_size parameters contain sensible values.
+  v.Check(f.Page > 0, "page", "must be greater than zero")
+  v.Check(f.Page <= 10_000_000, "page", "must be  maximum of 10 million")
+  v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+  v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
+  // Check that the sort parameter matches a value in the safelist.
+  v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), 
+    "sort", "invalid sort value")
 }
