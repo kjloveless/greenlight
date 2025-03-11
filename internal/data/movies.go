265c9051b8641f65e495af8ec6170c176d5bc34f
Kyle Loveless <kjloveless321@gmail.com>
2025-03-11 14:25:03 -0500

    sorting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
   "context"
   "database/sql"
   "errors"
+  "fmt"
   "time"
 
   "github.com/kjloveless/greenlight/internal/validator"
@@ -222,12 +223,15 @@ func (m MovieModel) GetAll(
   filters Filters,
 ) ([]*Movie, error) {
   // Construct the SQL query to retrieve all movie records.
-  query := `
+  // Add an ORDER BY clause and interpolate the sort column and direction.
+  // Importantly notice that we also include a secondary sort on the movie ID
+  // to ensure a consistent ordering.
+  query := fmt.Sprintf(`
     SELECT id, created_at, title, year, runtime, genres, version
     FROM movies
     WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
     AND (genres @> $2 OR $2 = '{}')
-    ORDER BY id`
+    ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
   // Create a context with a 3-second timeout.
   ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
