fab7db18efdf22f2a69d42c997d945b09b768547
Kyle Loveless <kjloveless321@gmail.com>
2025-03-13 13:58:47 -0500

    background welcome email

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -250,3 +250,19 @@ func (app *application) writeJSON(
 
   return nil
 }
+
+// The background() helper accepts an arbitrary function as a parameter.
+func (app *application) background(fn func()) {
+  // Launch a background goroutine.
+  go func() {
+    // Recover any panic.
+    defer func() {
+      if err := recover(); err != nil {
+        app.logger.Error(fmt.Sprintf("%v", err))
+      }
+    }()
+
+    // Execute the arbitrary function that we passed as the parameter.
+    fn()
+  }()
+}
