630cfab793a0d336fe121a0f2dd07c4e9fa07827
Kyle Loveless <kjloveless321@gmail.com>
2025-03-18 10:37:58 -0500

    typos, add api deploy to makefile

--- remote/setup/01.sh
+++ remote/setup/01.sh
@@ -55,8 +55,7 @@ ufw --force enable
 apt --yes install fail2ban
 
 # Install the migrate CLI tool.
-curl -L
-https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
 mv migrate.linux-amd64 /usr/local/bin/migrate
 
 # Install PostgreSQL
@@ -72,5 +71,16 @@ sudo -i -u postgres psql -d greenlight -c "CREATE ROLE greenlight WITH LOGIN PAS
 # environment variables in the /etc/environment file.
 echo "GREENLIGHT_DB_DSN='postgres://greenlight:${DB_PASSWORD}@localhost/greenlight'" >> /etc/environment
 
+# Install Caddy (see https://caddyserver.com/docs/install#debian-ubuntu-rasbian).
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
 
+# Upgrade all packages. Using the --force-confnew flag means that configuration
+# files will be replaced if newer ones are available.
+apt --yes -o Dpkg::Options::="--force-confnew" upgrade
 
+echo "Script complete! Rebooting..."
+reboot
